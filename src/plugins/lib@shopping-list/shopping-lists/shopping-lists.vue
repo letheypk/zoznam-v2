d888009ed50110a3e07201c7c5cca98159f76db9
letheypk <tomasmuellergm96@gmail.com>
2024-11-08 18:20:05 +0100

    zoznamv2
    prvy commit

---
+++ src/plugins/lib@shopping-list/shopping-lists/shopping-lists.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios'
+
+export default {
+	data() {
+		return {
+			shoppingLists: null
+		}
+	},
+
+	async mounted() {
+		try {
+			// const response = await axios.get('https://shoppinglist.wezeo.dev/shoppinglist/lists')
+			// const data = response.data.data
+
+			// Skrateny zapis zakomentovaneho kodu vyssie, kde vytiahneme data pomocou destrukcie objektu
+			const { data: { data: shoppingLists} } = await axios.get('/api/v1/shopping-lists')
+			this.shoppingLists = shoppingLists
+		} catch (error) {
+			console.error('Error:', error)
+			this.shoppingLists = { error }
+		}
+	}
+}
+</script>
